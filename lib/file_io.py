9735dab7c09dd5c456f21ac4a29b1b00673d475f
Tony <thunter1987@yahoo.com>
2023-08-14 19:33:08 -0500

    passes tests

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
-
+    open(file_name, 'w').write(file_content)
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as file_name:
+        file_name.write(file_content)
+        file_name.close()
+        
 def append_file(file_name, append_content):
-    pass
+    open(f'{file_name}.txt', 'a').write(append_content)
+    with open('file_name.txt', mode='a', encoding='utf-8') as file_name:
+        file_name.write(append_content)
 
 def read_file(file_name):
-    pass
+    file = open(f'{file_name}.txt', encoding='utf-8')
+    return file.read()
